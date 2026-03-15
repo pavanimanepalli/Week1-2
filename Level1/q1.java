3c0b22d563d356e892bd14b8dda9e6099b1540dc
Pavani-987 <lm4841@srmist.edu.in>
2026-03-15 23:05:39 +0530

    Added Level2 Question1

--- Level1/q1.java
+++ Level1/q1.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 public class q1 (
     public static void main(String[] args) {
 
@@ -7,5 +8,37 @@ public class q1 (
         int age = currentYear - birthYear;
 
         System.out.println("Harry's age in 2024 is " + age);
+=======
+// Question: Divide 14 pens among 3 students
+
+import java.util.Scanner;
+
+// Question: Basic Calculator Program
+public class q1_BasicCalculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Taking user inputs
+        System.out.print("Enter first number: ");
+        float number1 = sc.nextFloat();
+
+        System.out.print("Enter second number: ");
+        float number2 = sc.nextFloat();
+
+        // Arithmetic operations
+        float addition = number1 + number2;
+        float subtraction = number1 - number2;
+        float multiplication = number1 * number2;
+        float division = number1 / number2;
+
+        // Printing results
+        System.out.println("Addition = " + addition);
+        System.out.println("Subtraction = " + subtraction);
+        System.out.println("Multiplication = " + multiplication);
+        System.out.println("Division = " + division);
+
+        sc.close();
+>>>>>>> a2cffe0 (Added Level2 Question1)
     }
 }
---
+++ Level1/q5.java
@@ -0,0 +1,15 @@
+// Question: Divide 14 pens among 3 students
+
+public class q5_PensDistribution {
+    public static void main(String[] args) {
+
+        int totalPens = 14;
+        int students = 3;
+
+        int pensPerStudent = totalPens / students;
+        int remainingPens = totalPens % students;
+
+        System.out.println("The Pen Per Student is " + pensPerStudent +
+                           " and the remaining pen not distributed is " + remainingPens);
+    }
+}
